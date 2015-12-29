6c23f7c54901f4739b67744c5efdfb06fb636e2e
张鹏飞-芒果iOS <850944623@qq.com>
2015-12-29 11:02:48 +0800

    zhang

--- News/News/ViewController/NewsByZhangViewController.m
+++ News/News/ViewController/NewsByZhangViewController.m
@@ -17,6 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+    self.view.backgroundColor=[UIColor redColor];
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
